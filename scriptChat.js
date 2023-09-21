c8ff6af49530817926da1c46332fb345be9afbe2
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2023-09-21 17:33:14 -0500

    Update scriptChat.js

--- scriptChat.js
+++ scriptChat.js
@@ -1,6 +1,6 @@
 const allowedDomainsList = ['localhost', '127.0.0.1','bajajecuador.com','https://corporativo.curbe.com.ec/login']
 
-const APIDomain='http://apichatgpt.dev.curbe.com.ec/'
+const APIDomain='https://apichatgpt.dev.curbe.com.ec/'
 const RepositoryDomain='https://public.alexastudillo.com/'
 
 const css =document.createElement('link')
@@ -304,22 +304,29 @@ pie.appendChild(filaPieAud)
 
 const pdfContainer=document.createElement('pdf-container-chat')
 
-const pdfAntBtn= document.createElement('button-pdf-chat')
+const filaBtnPdf=document.createElement('row-chat')
 
-const pdfSigBtn= document.createElement('button-pdf-chat')
+const columnaAntPdf=document.createElement('col-chat')
+const btnAntPdf=document.createElement('button-chat')
+const imgAntPdf='<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="1em" height="1em" data-icon="check" aria-hidden="true"><path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path></svg>'
 
-const pdfZmIBtn= document.createElement('button-pdf-chat')
+const columnaPosPdf=document.createElement('col-chat')
+const btnPosPdf=document.createElement('button-chat')
+const imgPosPdf='<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="1em" height="1em" data-icon="check" aria-hidden="true"><path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path></svg>'
 
-const pdfZmOBtn= document.createElement('button-pdf-chat')
+const columnaZmInPdf=document.createElement('col-chat')
+const btnZmIn=document.createElement('button-chat')
+const imgZmIn=document.innerHTML='<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="1em" height="1em" data-icon="check" aria-hidden="true"><path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path></svg>'
+
+const columnaZmOutPdf=document.createElement('col-chat')
+const btnZmOut=document.createElement('button-chat')
+const imgZmOut=document.innerHTML='<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="1em" height="1em" data-icon="check" aria-hidden="true"><path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path></svg>'
 
 const pdfViewer=document.createElement('canvas')
 pdfViewer.classList.add('double-overflow')
 pdfViewer.id='pdf-canvas-chat'
 
 pdfContainer.appendChild(pdfViewer)
-
-
-
 
 const PDFStart = nameRoute => {           
     let loadingTask = pdfjsLib.getDocument(nameRoute),
@@ -328,50 +335,43 @@ const PDFStart = nameRoute => {
         ctx = canvas.getContext('2d'),
         scale = 1.5,
         numPage = 1;
-
-        const GeneratePDF = numPage => {
-
-            pdfDoc.getPage(numPage).then(page => {
-
-                let viewport = page.getViewport({ scale: scale });
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
-                
-                let renderContext = {
-                    canvasContext : ctx,
-                    viewport:  viewport
-                }
-
-                page.render(renderContext);
-            })
-            document.querySelector('#npages').innerHTML = numPage;
-
-        }
-
-        const PrevPage = () => {
-            if(numPage === 1){
-                return
+    const GeneratePDF = numPage => {
+        pdfDoc.getPage(numPage).then(page => {
+            let viewport = page.getViewport({ scale: scale });
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;                
+            let renderContext = {
+                canvasContext : ctx,
+                viewport:  viewport
             }
-            numPage--;
-            GeneratePDF(numPage);
+            page.render(renderContext);
+        })
+        document.querySelector('#npages').innerHTML = numPage;
+    }
+    const PrevPage = () => {
+        if(numPage === 1){
+            return
         }
+        numPage--;
+        GeneratePDF(numPage);
+    }
 
-        const NextPage = () => {
-            if(numPage >= pdfDoc.numPages){
-                return
-            }
-            numPage++;
-            GeneratePDF(numPage);
+    const NextPage = () => {
+        if(numPage >= pdfDoc.numPages){
+            return
         }
+        numPage++;
+        GeneratePDF(numPage);
+    }
 
-        document.querySelector('#prev').addEventListener('click', PrevPage)
-        document.querySelector('#next').addEventListener('click', NextPage )
+    document.querySelector('#prev').addEventListener('click', PrevPage)
+    document.querySelector('#next').addEventListener('click', NextPage )
 
-        loadingTask.promise.then(pdfDoc_ => {
-            pdfDoc = pdfDoc_;
-            document.querySelector('#npages').innerHTML = pdfDoc.numPages;
-            GeneratePDF(numPage)
-        });
+    loadingTask.promise.then(pdfDoc_ => {
+        pdfDoc = pdfDoc_;
+        document.querySelector('#npages').innerHTML = pdfDoc.numPages;
+        GeneratePDF(numPage)
+    });
 }
 
 const startPdf = () => {
