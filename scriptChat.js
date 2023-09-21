466d6f1e55add207d6a985ae4214392ad6f5d0ac
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2023-09-21 16:09:41 -0500

    accept domain

--- scriptChat.js
+++ scriptChat.js
@@ -1,4 +1,4 @@
-const allowedDomainsList = ['localhost', '127.0.0.1','bajajecuador.com']
+const allowedDomainsList = ['localhost', '127.0.0.1','bajajecuador.com','https://corporativo.curbe.com.ec/login']
 
 const APIDomain='http://apichatgpt.dev.curbe.com.ec/'
 const RepositoryDomain='https://public.alexastudillo.com/'
