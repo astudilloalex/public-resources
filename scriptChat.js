b70311ce33c6ef9ca88b68f72c1d0cd1367c83c0
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2023-10-05 09:10:46 -0500

    color error correction

--- scriptChat.js
+++ scriptChat.js
@@ -654,6 +654,7 @@ function downloadPDF(fileName,element){
 
     const columnaDescargar=document.createElement('col-chat')
     columnaDescargar.id='downloadPDF'
+    
     columnaDescargar.classList.add('col-4')
     columnaDescargar.classList.add('row-middle')
     columnaDescargar.classList.add('row-center')
@@ -661,7 +662,7 @@ function downloadPDF(fileName,element){
     const linkDesc= document.createElement('a')
     linkDesc.href=fileName
     linkDesc.download=fileName.substring(fileName.lastIndexOf('/')+1,fileName.indexOf(".pdf")+4)
-    linkDesc.style="color:#ffffff"
+    linkDesc.style="color:#000000"
     linkDesc.innerHTML='<svg viewBox="64 64 896 896" focusable="false" fill="currentColor" width="32px" height="40px"><path d="M505.7 661a8 8 0 0012.6 0l112-141.7c4.1-5.2.4-12.9-6.3-12.9h-74.1V168c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v338.3H400c-6.7 0-10.4 7.7-6.3 12.9l112 141.8zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"/></svg>'   
 
     columnaDescargar.appendChild(linkDesc)
